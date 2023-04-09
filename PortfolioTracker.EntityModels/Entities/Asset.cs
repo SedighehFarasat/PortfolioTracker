c23cae2c287b5051b07ecf1b96075059c00dcf53
Sedigheh <sd.farasat@gmail.com>
2023-04-09 16:57:39 +0430

    Add entity models to PortfolioTracker.EntityModels project

---
+++ PortfolioTracker.EntityModels/Entities/Asset.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioTracker.EntityModels.Entities;
+public class Asset
+{
+    public int Id { get; set; }
+    public string InstrumentId { get; set; }
+    public long Quantity { get; set; }
+    public decimal AveragePrice { get; set; }
+
+    public Instrument Instrument { get; set; }
+}
---
+++ PortfolioTracker.EntityModels/Entities/Instrument.cs
@@ -0,0 +1,21 @@
+﻿using PortfolioTracker.EntityModels.Enums;
+
+namespace PortfolioTracker.EntityModels.Entities;
+public class Instrument
+{
+    public Instrument()
+    {
+        TradingData = new HashSet<TradingData>();
+        Assets = new HashSet<Asset>();
+    }
+
+    public string Id { get; set; }
+    public string InsCode { get; set; }
+    public string Ticker { get; set; }
+    public string Name { get; set; }
+    public Board? Board { get; set; }
+    public Industry? Industry { get; set; }
+
+    public ICollection<TradingData> TradingData { get; set; }
+    public ICollection<Asset> Assets { get; set; }
+}
