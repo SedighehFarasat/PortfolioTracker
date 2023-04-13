a064a7ae7461a4e76317eb9c29c84c49b1ae0435
Sedigheh <sd.farasat@gmail.com>
2023-04-13 17:36:20 +0430

    Add TseService unit test

---
+++ PortfolioTracker.UnitTests/BackgroundTasksTests.cs
@@ -0,0 +1,24 @@
+﻿using PortfolioTracker.BackgroundTasks.Models;
+
+namespace PortfolioTracker.UnitTests;
+public class BackgroundTasksTests : IAsyncLifetime
+{
+    private Task<Trade> _data;
+
+    public async Task InitializeAsync()
+    {
+        var isin = "IRO1FKHZ0001";
+        await Task.Run(() => _data = BackgroundTasks.Services.TseService.FetchLiveData(isin));
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public void TseService()
+    {
+        Assert.NotNull(_data.Result);
+    }
+}
