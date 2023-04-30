cca24b1773e23599dbdd174ef551b5a2390dff85
Sedigheh <sd.farasat@gmail.com>
2023-04-30 21:53:37 +0430

    Clean up

--- PortfolioTracker.Mvc/Controllers/PortfolioController.cs
+++ PortfolioTracker.Mvc/Controllers/PortfolioController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using PortfolioTracker.Data;
 using PortfolioTracker.EntityModels.Entities;
 using PortfolioTracker.Mvc.Models;
